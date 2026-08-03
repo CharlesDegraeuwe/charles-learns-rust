33fbf045b9b967c1a8e0bc82de346d779ec0d1c4
CharlesDegraeuwe <charles.degraeuwe@icloud.com>
2026-08-03 02:16:35 +0200

    feat: chapter 3

---
+++ chapter-3/variables/src/main.rs
@@ -0,0 +1,8 @@
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 10;
+    println!("The value of x is: {}", x);
+    println!("The value of your the global value is: {}", THREE_HOURS_IN_SECONDS);
+}
